cfebc72372376c7406523c2fc69050683abf3dcc
WINDOWS-JNLT1TG\EDZ <xingyanlu_0511>
2020-12-16 21:38:30 +0800

    one1第一次修改

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      one1
+      one1
+      one1
+      one1
+      one1
   </div>
 </template>
 
